732355fd6d5ef9ce24c8a898d967373844678451
PaulAjii <paulajii134@gmail.com>
2025-03-30 23:14:07 +0100

    Add database population script and capitalize utility function

---
+++ server/populate.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import Users from './models/Users.js';
+import connectDB from './config/conn.js';
+import dotenv from 'dotenv';
+import capitalize from './utils/capitalize.js';
+dotenv.config();
+
+const populateDB = async () => {
+	try {
+		await connectDB(process.env.DB_URI);
+
+		// Fetch Data
+		const { data } = await axios.get(process.env.API_ENDPOINT);
+		const users = data.data;
+
+		const transformedUserData = users.map((user) => ({
+			studentId: user.matricNumber,
+			name: `${capitalize(user.firstName)} ${capitalize(user.lastName)}`,
+			password: user.matricNumber,
+			level: user.level,
+			set: user.classSet,
+		}));
+
+		await Users.deleteMany({});
+
+		await Users.insertMany(transformedUserData);
+
+		console.log('DataBase populated successfully!');
+		process.exit(0);
+
+		console.log(data);
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
+};
+populateDB();
