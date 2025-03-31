c9488d6dece5d1495307f397dbb4a75d60ffcdbf
PaulAjii <paulajii134@gmail.com>
2025-03-31 10:21:45 +0100

    Simplify password comparison method in user model

--- server/models/Users.js
+++ server/models/Users.js
@@ -96,7 +96,7 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.comparePassword = async function (password) {
 	try {
-		const isMatch = await bcrypt.compare(password, this.password);
+		return await bcrypt.compare(password, this.password);
 	} catch (err) {
 		throw new Error('Password do not match');
 	}
