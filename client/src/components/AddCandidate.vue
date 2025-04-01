9dd57955a7b9698ba88eafded530cbaec5cb46a8
Paul Ajijola <paulajii134@gmail.com>
2025-04-01 14:19:50 +0100

    Update AddCandidate.vue

--- client/src/components/AddCandidate.vue
+++ client/src/components/AddCandidate.vue
@@ -9,12 +9,13 @@
     <div class="modal-content">
       <h2>Add a New Candidate</h2>
       <form @submit.prevent="handleSubmit">
-        <div class="form-group">
+<div class="form__container">
+        <div class="form__group">
           <label for="name">Name</label>
           <input type="text" id="name" v-model="formData.name" placeholder="John Doe" required />
         </div>
 
-        <div class="form-group">
+        <div class="form__group">
           <label for="studentID">Student ID</label>
           <input
             type="text"
@@ -25,12 +26,12 @@
           />
         </div>
 
-        <div class="form-group">
+        <div class="form__group">
           <label for="alias">Alias</label>
           <input type="text" id="alias" v-model="formData.alias" placeholder="johnny" />
         </div>
 
-        <div class="form-group">
+        <div class="form__group">
           <label for="position">Position</label>
           <input
             type="text"
@@ -41,7 +42,7 @@
           />
         </div>
 
-        <div class="form-group">
+        <div class="form__group">
           <label for="level">Level</label>
           <select name="level" id="level" v-model="formData.level" required>
             <option value="" selected>Select Level</option>
@@ -52,7 +53,7 @@
           </select>
         </div>
 
-        <div class="form-group">
+        <div class="form__group">
           <label for="gender">Gender</label>
           <select name="gender" id="gender" v-model="formData.gender" required>
             <option value="" selected>Select Gender</option>
@@ -61,7 +62,7 @@
           </select>
         </div>
 
-        <div class="form-group">
+        <div class="form__group">
           <label for="catchPhrase">Catch Phrase</label>
           <textarea
             id="catchPhrase"
@@ -69,6 +70,7 @@
             placeholder="lorem ipsum..."
           ></textarea>
         </div>
+</div>
 
         <div class="form-actions">
           <button type="submit" class="submit__btn">Add Candidate</button>
@@ -184,5 +186,65 @@ button {
 .cancel__btn {
   background: #f44336;
   color: white;
+}
+
+.form__container {
+  width: 100%;
+  max-width: 600px;
+  padding: 1.5rem 0.5rem 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 2rem;
+}
+
+.form__group {
+  display: flex;
+  gap: 0.5rem;
+  border: 2px solid rgb(17, 17, 17);
+  border-radius: 0.25rem;
+  font-size: 1rem;
+}
+
+.form__group > * {
+  font-size: 0.95rem;
+  font-weight: 500;
+  letter-spacing: 1px;
+}
+
+.form__group > label {
+  background-color: rgb(17, 17, 17);
+  color: var(--neutral);
+  min-width: 7rem;
+  padding-inline: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.form__group > input {
+  padding-block: 0.35rem;
+  width: 100%;
+}
+
+.submit__btn {
+  background-color: rgb(37, 37, 238);
+  color: var(--neutral);
+  padding-block: 0.5rem;
+  width: 100%;
+  max-width: 600px;
+  font-size: 1rem;
+  font-weight: 600;
+  transition: all 0.5s ease;
+}
+
+.submit__btn:hover {
+  border-radius: 1rem 0 1rem 0;
+}
+
+.submit__btn:disabled {
+  opacity: 0.7;
+  cursor: not-allowed;
+  background-color: rgb(128, 128, 128);
 }
 </style>
