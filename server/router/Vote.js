cdc4994f5506654255e3d27decd34bcd1b6c2d15
Paul Ajijola <paulajii134@gmail.com>
2025-04-05 17:27:15 +0100

    Update Vote.js

--- server/router/Vote.js
+++ server/router/Vote.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { castVote, getResults } from '../controllers/Vote.js';
-import { isVoter } from '../middlewares/isVoter.js';
+import { isAdmin } from '../middlewares/isAdmin.js';
 import { authenticateUser } from '../middlewares/auth.js';
 
 const router = Router();
 
-router.post('/cast', authenticateUser, isVoter, castVote);
+router.post('/cast', authenticateUser, isAdmin, castVote);
 router.route('/results').get(getResults);
 
 export default router;
